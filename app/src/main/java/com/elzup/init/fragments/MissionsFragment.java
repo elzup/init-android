40bbd2c05553d0455e58560b33ff6f81941eac9f
elzup <guild0105@gmail.com>
2016-12-10 15:02:48 +0900

    Fix Login subscribe

--- app/src/main/java/com/elzup/init/fragments/MissionsFragment.java
+++ app/src/main/java/com/elzup/init/fragments/MissionsFragment.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -116,6 +117,8 @@ public class MissionsFragment extends Fragment implements OnRecyclerListener {
                         missionEntities.addAll(items);
                         adapter.notifyDataSetChanged();
                     }
+                }, throwable -> {
+                    Log.e(TAG, "initData: ", throwable);
                 });
     }
 }
